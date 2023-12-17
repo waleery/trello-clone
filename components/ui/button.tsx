171b2ec4ed8d603c966ac2b482e46041e39dfab9
waleery <walenda1997@gmail.com>
2023-12-18 00:28:17 +0100

    Added comments to button component and utils.ts

--- components/ui/button.tsx
+++ components/ui/button.tsx
@@ -1,13 +1,23 @@
 import * as React from "react"
+
+//A component for handling slots, which are dynamic content insertion points.
 import { Slot } from "@radix-ui/react-slot"
+
+//Tools for handling component variants.
 import { cva, type VariantProps } from "class-variance-authority"
 
+//A function for combining CSS classes in a flexible manner.
 import { cn } from "@/lib/utils"
 
+
+
+// Define button variants using class-variance-authority
 const buttonVariants = cva(
+  // Base class for styling common properties
   "inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50",
   {
     variants: {
+      // Styles for different button variants
       variant: {
         default: "bg-primary text-primary-foreground hover:bg-primary/90",
         destructive:
@@ -19,6 +29,7 @@ const buttonVariants = cva(
         ghost: "hover:bg-accent hover:text-accent-foreground",
         link: "text-primary underline-offset-4 hover:underline",
       },
+      // Styles for different button sizes
       size: {
         default: "h-10 px-4 py-2",
         sm: "h-9 rounded-md px-3",
@@ -26,6 +37,7 @@ const buttonVariants = cva(
         icon: "h-10 w-10",
       },
     },
+    // Default variants for the button
     defaultVariants: {
       variant: "default",
       size: "default",
@@ -36,6 +48,7 @@ const buttonVariants = cva(
 export interface ButtonProps
   extends React.ButtonHTMLAttributes<HTMLButtonElement>,
     VariantProps<typeof buttonVariants> {
+  // Additional prop to render the button as a child (using Slot)
   asChild?: boolean
 }
 
@@ -51,6 +64,8 @@ const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
     )
   }
 )
+
+// Set a display name for the Button component
 Button.displayName = "Button"
 
 export { Button, buttonVariants }
--- lib/utils.ts
+++ lib/utils.ts
@@ -1,6 +1,7 @@
 import { type ClassValue, clsx } from "clsx"
 import { twMerge } from "tailwind-merge"
  
+//function to merge css classes
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }

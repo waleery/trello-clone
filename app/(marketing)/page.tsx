735c3706842ee2e41db7850c649fdd802a0ef6c7
waleery <walenda1997@gmail.com>
2023-12-18 23:31:16 +0100

    Changed fonts for heading and text

--- app/(marketing)/page.tsx
+++ app/(marketing)/page.tsx
@@ -1,29 +1,48 @@
 import { Medal } from "lucide-react";
-
-import { Button } from "@/components/ui/button";
 import Link from "next/link";
+import localFont from "next/font/local";
+import { Poppins } from "next/font/google";
 
+import { cn } from "@/lib/utils";
+import { Button } from "@/components/ui/button";
+
+const headingFont = localFont({
+    src: "../../public/fonts/font.woff2",
+});
+
+const textFont = Poppins({
+    subsets: ["latin"],
+    weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"]
+});
 
 const MarketingPage = () => {
     return (
         <div className="flex items-center justify-center flex-col">
-            <div className="flex items-center justify-center flex-col">
+            <div
+                className={cn(
+                    "flex items-center justify-center flex-col",
+                    headingFont.className
+                )}
+            >
                 <div className="mb-4 flex items-center border shadow-sm p-4 bg-amber-100 text-amber-700 rounded-full uppercase">
                     <Medal className="h-6 w-6 mr-2" />
                     Your Gateway to Achievements
                 </div>
                 <h1 className="text-3xl md:text-6xl text-center text-neutral-800 mb-6">
-                Welcome to Achiveo - Where goals 
+                    Welcome to Achiveo - Where goals
                 </h1>
                 <div className="text-3xl md:text-6xl bg-gradient-to-r from-fuchsia-600 to-pink-600 text-white px-4 p-2 rounded-md pb-3 w-fit">
                     become realities.
                 </div>
             </div>
-            <div className="text-sm md:text-xl text-neutral-400 mt-4 max-w-xs md:max-w-2xl text-center mx-auto">
-            Foster teamwork, organize projects effortlessly, and unlock new productivity levels - your perfect companion for achieving goals is Achievo.
-
+            <div className={cn("text-sm md:text-xl text-neutral-400 mt-4 max-w-xs md:max-w-2xl text-center mx-auto", textFont.className)}>
+                Foster teamwork, organize projects effortlessly, and unlock new
+                productivity levels - your perfect companion for achieving goals
+                is Achievo.
             </div>
-            <Button className="mt-6" size='lg' asChild><Link href='/sign-up'>Get Taskify for free</Link></Button>
+            <Button className="mt-6" size="lg" asChild>
+                <Link href="/sign-up">Get Taskify for free</Link>
+            </Button>
         </div>
     );
 };

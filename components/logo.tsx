5ae48a3377ce70cbc9fe9d471057f672d4464604
waleery <walenda1997@gmail.com>
2023-12-18 23:55:45 +0100

    Added logo component

---
+++ components/logo.tsx
@@ -0,0 +1,21 @@
+import { cn } from "@/lib/utils";
+import Image from "next/image";
+import Link from "next/link";
+import localFont from "next/font/local";
+
+const headingFont = localFont({
+    src: "../public/fonts/font.woff2",
+});
+
+const Logo = () => {
+    return (
+        <Link href="/">
+            <div className="hover:opacity items-center gap-x-2 hidden md:flex">
+                <Image src="/logo.svg" alt="Logo" height={30} width={30} />
+                <p className={cn("text-lg text-neutral-700 pb-1", headingFont.className)}>Achiveo</p>
+            </div>
+        </Link>
+    );
+};
+
+export default Logo;

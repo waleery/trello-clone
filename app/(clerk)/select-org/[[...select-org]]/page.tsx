d25eeae37af4d89314a803f85a259bf52e1cd3c0
waleery <walenda1997@gmail.com>
2024-01-03 17:10:59 +0100

    Added redirect after select/create organization

--- app/(clerk)/select-org/[[...select-org]]/page.tsx
+++ app/(clerk)/select-org/[[...select-org]]/page.tsx
@@ -1,9 +1,11 @@
 import { OrganizationList } from "@clerk/nextjs";
 
-
-
 export default function CreateOrganizationPage() {
-    return(
-        <OrganizationList />
-    )
+    return (
+        <OrganizationList
+            hidePersonal
+            afterSelectOrganizationUrl="/organization/:id"
+            afterCreateOrganizationUrl="/organization/:id"
+        />
+    );
 }

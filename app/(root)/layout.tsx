4e8ae9a8a4750c1ee3b93cf092fb18ef5645a679
waleery <walenda1997@gmail.com>
2024-01-03 16:27:31 +0100

    Refactored project structure

--- app/(root)/layout.tsx
+++
@@ -1,7 +0,0 @@
-import { ClerkProvider } from "@clerk/nextjs";
-import { ReactNode } from "react";
-
-const RootLayout = ({ children }: { children: ReactNode }) => {
-    return <ClerkProvider>{children}</ClerkProvider>;
-};
-export default RootLayout;

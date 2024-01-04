09f665866248754afcacaef8d98243c122525bd4
waleery <walenda1997@gmail.com>
2024-01-04 23:26:30 +0100

    Refactored OrganizationControl logic

--- app/(dashboard)/organization/[organizationId]/_components/organizationControl.tsx
+++ app/(dashboard)/organization/[organizationId]/_components/organizationControl.tsx
@@ -1,19 +1,24 @@
 "use client";
 
-import { useOrganizationList } from "@clerk/nextjs";
+import { useOrganization, useOrganizationList } from "@clerk/nextjs";
 import { useParams } from "next/navigation";
 import { useEffect } from "react";
 
 const OrganizationControl = () => {
     const params = useParams();
     const { setActive } = useOrganizationList();
+    const { organization: activeOrganization } = useOrganization();
+
     useEffect(() => {
         if (!setActive) return;
 
-        setActive({
-            organization: params.organizationId as string,
-        });
-    }, [setActive, params.organizationId]);
+        if (activeOrganization && params.organizationId !== activeOrganization?.id) {
+            setActive({
+                organization: params.organizationId as string,
+            });
+        }
+        
+    }, [setActive, params.organizationId, activeOrganization]);
     return null;
 };
 export default OrganizationControl;

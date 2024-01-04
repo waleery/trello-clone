7fa20560881a4e3c1418964e4376a1025de7def8
waleery <walenda1997@gmail.com>
2024-01-04 20:26:21 +0100

    Created NavItem component and puted in Sidebar

--- app/(dashboard)/_components/sidebar.tsx
+++ app/(dashboard)/_components/sidebar.tsx
@@ -1,15 +1,19 @@
 "use client";
 
+import { Accordion } from "@/components/ui/accordion";
+import { Button } from "@/components/ui/button";
 import { Skeleton } from "@/components/ui/skeleton";
 import { useOrganization, useOrganizationList } from "@clerk/nextjs";
+import { Plus } from "lucide-react";
 import Link from "next/link";
 import { useLocalStorage } from "usehooks-ts";
+import {NavItem, Organization} from "./nav-item";
 
 interface SidebarProps {
     storageKey?: string;
 }
 
-const Sidebar = ({ storageKey = "achiveo-sidebar-project" }) => {
+const Sidebar = ({ storageKey = "achiveo-sidebar-project" } : SidebarProps) => {
     const [expanded, setExpanded] = useLocalStorage<Record<string, any>>(
         storageKey,
         {}
@@ -25,6 +29,7 @@ const Sidebar = ({ storageKey = "achiveo-sidebar-project" }) => {
             },
         });
 
+    // { "123": true, "456": false, "789": true} => ["123", "789"]
     const defaultAccordionValue: string[] = Object.keys(expanded).reduce(
         (acc: string[], key: string) => {
             if (expanded[key]) {
@@ -41,11 +46,11 @@ const Sidebar = ({ storageKey = "achiveo-sidebar-project" }) => {
             [id]: !expanded[id],
         }));
     };
-    console.log("-------")
-    console.log(isLoadedOrganization)
-    console.log(activeOrganization)
-    console.log(isLoadedOrganizationList)
-    console.log(userMemberships)
+    console.log("-------");
+    console.log(isLoadedOrganization);
+    console.log(activeOrganization);
+    console.log(isLoadedOrganizationList);
+    console.log(userMemberships);
 
     if (
         !isLoadedOrganization ||
@@ -58,8 +63,40 @@ const Sidebar = ({ storageKey = "achiveo-sidebar-project" }) => {
             </>
         );
     }
-    
-    return <div>Sidebar </div>;
+
+    return (
+        <>
+            <div className="font-medium text-xs flex items-center mb-1">
+                <span className="pl-4">Workspaces</span>
+                <Button
+                    asChild
+                    type="button"
+                    size="icon"
+                    variant="ghost"
+                    className="ml-auto"
+                >
+                    <Link href="/select-org">
+                        <Plus className="h-4 w-4 " />
+                    </Link>
+                </Button>
+            </div>
+            <Accordion
+                type="multiple"
+                defaultValue={defaultAccordionValue}
+                className="space-y-2"
+            >
+                {userMemberships.data.map(({ organization }) => (
+                    <NavItem
+                        key={organization.id}
+                        isActive={activeOrganization?.id === organization.id}
+                        isExpanded={expanded[organization.id]}
+                        organization={organization as Organization}
+                        onExpand={onExpand}
+                    />
+                ))}
+            </Accordion>
+        </>
+    );
 };
 
 export default Sidebar;

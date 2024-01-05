5d70427db31befc214ea2db0cd8128ddf8c8aa31
waleery <walenda1997@gmail.com>
2024-01-05 22:38:27 +0100

    Created zustand store to manage MobileSidebar state

---
+++ hooks/useMobileSidebar.ts
@@ -0,0 +1,35 @@
+import { StateCreator, create } from "zustand";
+
+// Define the interface describing the shape of the store state.
+interface MobileSidebarStore {
+    isOpen: boolean;  
+    onOpen: () => void;
+    onClose: () => void;  
+}
+
+
+// Define a StateCreator function to create the initial store state.
+
+// : StateCreator<MobileSidebarStore>: Specifies the type of this constant. 
+// The StateCreator type is a kind of function that creates and manages the application state. 
+// <MobileSidebarStore> is the type of state that this function will create.
+
+const store: StateCreator<MobileSidebarStore> = (set) => ({
+    isOpen: false,
+    onOpen: () => set({ isOpen: true }),
+    onClose: () => set({ isOpen: false }),
+});
+
+
+// Create an instance of the store using the create function.
+export const useMobileSidebar = create<MobileSidebarStore>()(store);
+
+
+//https://github.com/pmndrs/zustand/blob/main/docs/guides/typescript.md
+
+// In general, empty parentheses are used for immediately invoking the create
+// <MobileSidebarStore>() function. This works because create<MobileSidebarStore> 
+// returns a function, and we immediately invoke it with empty parentheses to obtain 
+// a ready instance of the store. This ensures that types are correctly set for 
+// both the store and its state.
+

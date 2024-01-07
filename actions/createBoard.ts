d246cd14e9376f83805615e31de9766f370dba74
waleery <walenda1997@gmail.com>
2024-01-07 23:15:35 +0100

    feat(serverActions): add data validation for creating a new board

--- actions/createBoard.ts
+++ actions/createBoard.ts
@@ -1,13 +1,23 @@
-'use server'
+"use server";
 
-import { db } from "@/lib/db"
+import { db } from "@/lib/db";
+import { revalidatePath } from "next/cache";
+import { z } from "zod";
+
+const CreateBoard = z.object({
+    title: z.string(),
+});
 
 export const create = async (formData: FormData) => {
-    const title = formData.get('title') as string
+    const { title } = CreateBoard.parse({
+        title: formData.get("title"),
+    });
 
     await db.board.create({
-        data:{
-          title,  
-        }
-    })
-}
+        data: {
+            title,
+        },
+    });
+
+    revalidatePath('/organization/org_2aSczl0vMsYIJj39JE3N97BK4Kw')
+};

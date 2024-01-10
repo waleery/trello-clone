b4864fb1966cec0dfa55421f93beb296fc27233e
waleery <walenda1997@gmail.com>
2024-01-10 20:44:22 +0100

    feat(organization): add useFormStatus hook to FormInput component
    change 'input' from create board to 'input' from shadcn

--- app/(dashboard)/organization/[organizationId]/formInput.tsx
+++ app/(dashboard)/organization/[organizationId]/formInput.tsx
@@ -1,18 +1,23 @@
+import { Input } from "@/components/ui/input";
+import { useFormStatus } from "react-dom";
+
 interface FormInputProps {
     errors?: {
         title?: string[];
-    }
+    };
 }
 
-const FormInput = ({errors} : FormInputProps) => {
+const FormInput = ({ errors }: FormInputProps) => {
+    const { pending } = useFormStatus();
+
     return (
         <div>
-            <input
+            <Input  
                 id="title"
                 name="title"
                 required
                 placeholder="Enter a board title"
-                className="border-black border p-1"
+                disabled={pending}
             />
             {errors?.title ? (
                 <div>

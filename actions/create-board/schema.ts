1a8732ae37eae87112fc1cdfc7593f069c99fb82
waleery <walenda1997@gmail.com>
2024-01-17 22:15:45 +0100

    fix(serverActions): add validation for required title field

--- actions/create-board/schema.ts
+++ actions/create-board/schema.ts
@@ -3,11 +3,15 @@ import { z } from "zod";
 export const CreateBoard = z.object({
     title: z
         .string({
+            //this checks if there is any value
             required_error: "Title is required",
             invalid_type_error: "Title must be a string",
         })
         .min(3, {
             message: "Title must be at least 3 characters long",
+        })
+        .refine(value => value !== '', {
+            message: "Title is required",
         }),
     image: z
         .string({

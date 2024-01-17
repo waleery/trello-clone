3ffcd46e264cfc0196ff79ecc369093c05290546
waleery <walenda1997@gmail.com>
2024-01-17 11:57:08 +0100

    feat(organization): add info component
    which is displayed in OrganizationIdPage

--- app/(dashboard)/organization/[organizationId]/page.tsx
+++ app/(dashboard)/organization/[organizationId]/page.tsx
@@ -1,8 +1,9 @@
+import { Info } from "./_components/info";
 
 const OrganizationIdPage = async () => {
     return (
-        <div className="w-full flex-col space-y-4">
-
+        <div className="w-full mb-20">
+            <Info/>
         </div>
     );
 };

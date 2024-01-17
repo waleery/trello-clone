197cffade0654ad9a9c24e9ccd8cea12b5115e2c
waleery <walenda1997@gmail.com>
2024-01-17 22:58:36 +0100

    feat(organization): add skeleton loading for board list component

--- app/(dashboard)/organization/[organizationId]/_components/boardList.tsx
+++ app/(dashboard)/organization/[organizationId]/_components/boardList.tsx
@@ -5,6 +5,7 @@ import { auth } from "@clerk/nextjs";
 import { redirect } from "next/navigation";
 import { db } from "@/lib/db";
 import Link from "next/link";
+import { Skeleton } from "@/components/ui/skeleton";
 
 
 export const BoardList = async () => {
@@ -60,3 +61,17 @@ export const BoardList = async () => {
         </div>
     );
 };
+
+BoardList.Skeleton = function SkeletonBoardList() {
+    return(
+        <div className="grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-4 gap-4">
+            <Skeleton className="aspect-video h-full w-full p-2" />
+            <Skeleton className="aspect-video h-full w-full p-2" />
+            <Skeleton className="aspect-video h-full w-full p-2" />
+            <Skeleton className="aspect-video h-full w-full p-2" />
+            <Skeleton className="aspect-video h-full w-full p-2" />
+            <Skeleton className="aspect-video h-full w-full p-2" />
+            <Skeleton className="aspect-video h-full w-full p-2" />
+        </div>
+    )
+}
--- app/(dashboard)/organization/[organizationId]/page.tsx
+++ app/(dashboard)/organization/[organizationId]/page.tsx
@@ -1,16 +1,18 @@
 import { Separator } from "@/components/ui/separator";
 import { Info } from "./_components/info";
 import { BoardList } from "./_components/boardList";
+import { Suspense } from "react";
 
 const OrganizationIdPage = async () => {
     return (
         <div className="w-full mb-20">
-            <Info/>
-            <Separator className="my-4"/>
+            <Info />
+            <Separator className="my-4" />
             <div className="px-2 md:px-4">
-                <BoardList/>
+                <Suspense fallback={<BoardList.Skeleton />}>
+                    <BoardList />
+                </Suspense>
             </div>
-
         </div>
     );
 };

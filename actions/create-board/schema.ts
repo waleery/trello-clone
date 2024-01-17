cd9eb4bf547a7b681d938c1049235d35bfb7eac4
waleery <walenda1997@gmail.com>
2024-01-17 20:53:26 +0100

    refactor(zod): refactor CreateBoard schema to include image field

--- actions/create-board/schema.ts
+++ actions/create-board/schema.ts
@@ -1,11 +1,17 @@
 import { z } from "zod";
 
 export const CreateBoard = z.object({
-    title: z.string({
-        required_error: "Title is required",
-        invalid_type_error: "Title must be a string",
-    }).min(3,{
-        message: "Title must be at least 3 characters long",
-    }),
-    
-})
+    title: z
+        .string({
+            required_error: "Title is required",
+            invalid_type_error: "Title must be a string",
+        })
+        .min(3, {
+            message: "Title must be at least 3 characters long",
+        }),
+    image: z
+        .string({
+            required_error: "Image is required",
+            invalid_type_error: "Image must be a string",
+        })
+});

6ffdf1469c83b37b62c7dd37ed36b2c2143f614d
waleery <walenda1997@gmail.com>
2024-01-17 22:54:42 +0100

    feat(organization): Add dynamic boardList rendering and authentication check

--- app/(dashboard)/organization/[organizationId]/_components/boardList.tsx
+++ app/(dashboard)/organization/[organizationId]/_components/boardList.tsx
@@ -1,8 +1,26 @@
 import { HelpCircle, User2 } from "lucide-react";
 import { Hint } from "@/components/hint";
 import { FormPopover } from "@/components/form/formPopover";
+import { auth } from "@clerk/nextjs";
+import { redirect } from "next/navigation";
+import { db } from "@/lib/db";
+import Link from "next/link";
 
-export const BoardList = () => {
+
+export const BoardList = async () => {
+    const { orgId } = auth();
+
+    if (!orgId) return redirect("/select-org");
+
+    const boards = await db.board.findMany({
+        where: {
+            orgId,
+        },
+        orderBy: {
+            createdAt: "desc",
+        },
+    });
+
     return (
         <div className="space-y-4">
             <div className="flex items-center font-semibold text-lg text-neutral-700">
@@ -10,6 +28,19 @@ export const BoardList = () => {
                 Your boards
             </div>
             <div className="grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-4 gap-4">
+                {boards.map((board) => (
+                    <Link
+                        key={board.id}
+                        href={`/board/${board.id}`}
+                        style={{
+                            backgroundImage: `url(${board.imageThumbUrl})`,
+                        }}
+                        className="group relative aspect-video bg-no-repeat bg-center bg-cover bg-sky-700 rounded-sm h-full w-full p-2 overflow-hidden"
+                    >
+                        <div className="absolute inset-0 group-hover:bg-white/20 transition" />
+                        <p className="relative font-semibold text-white">{board.title}</p>
+                    </Link>
+                ))}
                 <FormPopover side="right" sideOffset={10}>
                     <div
                         role="button"

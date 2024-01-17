2036220055dfef950091d4d1a46c45ac1bf53531
waleery <walenda1997@gmail.com>
2024-01-17 12:33:18 +0100

    feat(organization): add boardList component

---
+++ app/(dashboard)/organization/[organizationId]/_components/boardList.tsx
@@ -0,0 +1,12 @@
+import { User2 } from "lucide-react";
+
+export const BoardList = () => {
+    return (
+        <div className="space-y-4">
+            <div className="flex items-center font-semibold text-lg text-neutral-700">
+                <User2 className="h-6 w-6 mr-2" />
+                Your boards
+            </div>
+        </div>
+    );
+};

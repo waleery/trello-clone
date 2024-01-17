a61feda4dc9af153fbe73f136a6cabf01e18441a
waleery <walenda1997@gmail.com>
2024-01-17 12:34:15 +0100

    feat(organization): add "Create new board" button to BoardList component

--- app/(dashboard)/organization/[organizationId]/_components/boardList.tsx
+++ app/(dashboard)/organization/[organizationId]/_components/boardList.tsx
@@ -7,6 +7,15 @@ export const BoardList = () => {
                 <User2 className="h-6 w-6 mr-2" />
                 Your boards
             </div>
+            <div className="grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-4 gap-4">
+                <div
+                    role="button"
+                    className="aspect-video relative h-full w-full bg-muted rounded-sm flex flex-col gap-y-1 items-center justify-center hover:opacity-75 transition"
+                >
+                    <p className="text-sm">Create new board</p>
+                    <span className="text-xs">5 remaining</span>
+                </div>
+            </div>
         </div>
     );
 };

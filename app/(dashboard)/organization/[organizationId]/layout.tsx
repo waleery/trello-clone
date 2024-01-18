35dffd50fb2f41e468d9c51d038f94fedaeb27ba
waleery <walenda1997@gmail.com>
2024-01-18 20:38:21 +0100

    feat(organization): add generateMetadata function to layout.tsx

--- app/(dashboard)/organization/[organizationId]/layout.tsx
+++ app/(dashboard)/organization/[organizationId]/layout.tsx
@@ -1,5 +1,17 @@
+import {startCase} from 'lodash'
+
 import { ReactNode } from "react";
 import OrganizationControl from "./_components/organizationControl";
+import { auth } from '@clerk/nextjs';
+
+export async function generateMetadata(){
+    const {orgSlug} = auth()
+
+    return {
+        title: startCase(orgSlug || 'Organization')
+    }
+}
+
 
 const OrganizationIdLayout = ({ children }: { children: ReactNode }) => {
     return (

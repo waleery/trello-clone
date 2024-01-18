7cbfda0af68875d38f9b85ac925c59a03145130b
waleery <walenda1997@gmail.com>
2024-01-18 22:45:14 +0100

    feat(board): add board page and layout components

---
+++ app/(dashboard)/board/[boardId]/layout.tsx
@@ -0,0 +1,27 @@
+import { db } from "@/lib/db";
+import { auth } from "@clerk/nextjs";
+import { notFound, redirect } from "next/navigation";
+import { ReactNode } from "react";
+
+const BoardIdLayout = async ({ children, params }: { children: ReactNode, params: {boardId: string} }) => {
+    const {orgId} = auth()
+
+    if(!orgId) return redirect("/select-org")
+    
+     const board = await db.board.findUnique({
+        where:{
+            id: params.boardId,
+            orgId
+        }
+     })
+     if (!board){
+        notFound()
+     }
+    return (
+        <div className="relative h-full bg-no-repeat bg-cover bg-center" style={{ backgroundImage: `url(${board.imageFullUrl})`}}>
+            <main className="relative pt-28 h-full">{children}</main>
+        </div>
+    );
+};
+
+export default BoardIdLayout;
---
+++ app/(dashboard)/board/[boardId]/page.tsx
@@ -0,0 +1,6 @@
+const BoardIdPage = () => {
+  return (
+    <div>BoardIdPage</div>
+  )
+}
+export default BoardIdPage

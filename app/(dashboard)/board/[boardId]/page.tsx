fe2e928945ec786417a9fedbdd90cb279faf8fee
waleery <walenda1997@gmail.com>
2024-01-19 21:42:05 +0100

    feat(board): fetch and display lists based on boardId and orgId

--- app/(dashboard)/board/[boardId]/page.tsx
+++ app/(dashboard)/board/[boardId]/page.tsx
@@ -1,6 +1,40 @@
-const BoardIdPage = () => {
-  return (
-    <div>BoardIdPage</div>
-  )
+import { db } from "@/lib/db";
+import { auth } from "@clerk/nextjs";
+import { redirect } from "next/navigation";
+
+interface BoardIdPageProps {
+    params: {
+        boardId: string;
+    };
 }
-export default BoardIdPage
+
+const BoardIdPage = async ({ params }: BoardIdPageProps) => {
+    const { orgId } = auth();
+
+    if (!orgId) redirect("/select-org");
+
+    const lists = await db.list.findMany({
+        where: {
+            boardId: params.boardId,
+            board: {
+                orgId: orgId,
+            },
+        },
+        include: {
+            cards: {
+                orderBy: {
+                    order: "asc",
+                },
+            },
+        },
+        orderBy: {
+            order: "asc",
+        },
+    });
+    return (
+        <div className="p-4 h-full overflow-x-auto">
+            <ListContainer boardId={params.boardId} data={lists}/>
+        </div>
+    );
+};
+export default BoardIdPage;

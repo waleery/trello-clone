45dc4b94fe81953644a505f54fcf66ba931d4e24
waleery <walenda1997@gmail.com>
2024-01-19 17:42:35 +0100

    style(board): update boardNavbar styling

--- app/(dashboard)/board/[boardId]/_components/boardNavbar.tsx
+++ app/(dashboard)/board/[boardId]/_components/boardNavbar.tsx
@@ -9,7 +9,7 @@ interface BoardNavbarProps {
 
 const BoardNavbar = async ({ data }: BoardNavbarProps) => {
     return (
-        <div className="w-full h-14 z-[40] bg-black/50 fixed top-14 flex items-center px-6 gap-x-4 text-white">
+        <div className="w-full h-fit min-h-[3.5rem] py-2 z-[40] bg-black/50 fixed top-14 flex items-center px-6 gap-x-4 text-white">
             <BoardTitleForm data={data} />
         </div>
     );

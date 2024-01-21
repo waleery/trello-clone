b1f90874f70e8e233dd28ac6457c53c9d77449d6
waleery <walenda1997@gmail.com>
2024-01-21 18:24:21 +0100

    feat(card): add FormTextArea to CardForm component

--- app/(dashboard)/board/[boardId]/_components/cardForm.tsx
+++ app/(dashboard)/board/[boardId]/_components/cardForm.tsx
@@ -1,7 +1,9 @@
 "use client";
 
+import { FormSubmit } from "@/components/form/formSubmit";
+import { FormTextArea } from "@/components/form/formTextArea";
 import { Button } from "@/components/ui/button";
-import { Plus } from "lucide-react";
+import { Plus, X } from "lucide-react";
 import { forwardRef } from "react";
 
 interface CardFormProps {
@@ -12,9 +14,36 @@ interface CardFormProps {
 }
 export const CardForm = forwardRef<HTMLTextAreaElement, CardFormProps>(
     ({ listId, isEditing, enableEditing, disableEditing }, ref) => {
+        if (isEditing) {
+            return (
+                <form className="m-1 py-0.5 px-1 space-y-4">
+                    <FormTextArea
+                        id="title"
+                        onKeyDown={() => {}}
+                        ref={ref}
+                        placeholder="Enter a title for new card..."
+                    />
+                    <input hidden id="listId" name="listId" value={listId} />
+                    <div className="flex items-center gap-x-1" >
+                        <FormSubmit>
+                            Add card
+                        </FormSubmit>
+                        <Button onClick={disableEditing} size="sm" variant="ghost">
+                            <X className="h-5 w-5"/>
+                        </Button>
+                    </div>
+                </form>
+            );
+        }
+
         return (
             <div className="pt-2 px-2">
-                <Button onClick={enableEditing} className="h-auto px-2 py-1.5 w-full justify-start text-muted-foreground text-sm" size='sm' variant="ghost">
+                <Button
+                    onClick={enableEditing}
+                    className="h-auto px-2 py-1.5 w-full justify-start text-muted-foreground text-sm"
+                    size="sm"
+                    variant="ghost"
+                >
                     <Plus className="h-4 w-4 mr-2" />
                     Add a card
                 </Button>

a1221507451fc3dc01191fa084ac71114b0c038f
waleery <walenda1997@gmail.com>
2024-01-21 11:58:44 +0100

    feat(lists): add ListItem and ListHeader components

--- app/(dashboard)/board/[boardId]/_components/listContainer.tsx
+++ app/(dashboard)/board/[boardId]/_components/listContainer.tsx
@@ -3,6 +3,8 @@
 import { ListWithCards } from "@/types";
 import { List } from "@prisma/client";
 import ListForm from "./listForm";
+import { useEffect, useState } from "react";
+import ListItem from "./listItem";
 
 interface ListContainerProps {
     data: ListWithCards[];
@@ -10,8 +12,24 @@ interface ListContainerProps {
 }
 
 const ListContainer = ({ data, boardId }: ListContainerProps) => {
+    const [orderedData, setOrderedData] = useState<ListWithCards[]>(data);
+
+    useEffect(() => {
+        setOrderedData(data);
+    },[data])
+
+
     return (
-        <ol className="border-yellow-500 border">
+        <ol className="flex gap-x-3 h-full">
+            {orderedData.map((list, index) =>{
+               return (
+                <ListItem
+                    key={list.id}
+                    data={list}
+                    index={index}
+                />
+               ) 
+            })}
             <ListForm/>
             <div className="flex-shrink-0 w-1  border-green-500 border"/>
         </ol>
---
+++ app/(dashboard)/board/[boardId]/_components/listItem.tsx
@@ -0,0 +1,20 @@
+"use client";
+
+import { ListWithCards } from "@/types";
+import ListHeader from "./listHeader";
+
+interface ListItemProps {
+    data: ListWithCards;
+    index: number;
+}
+
+const ListItem = ({ data, index }: ListItemProps) => {
+    return (
+        <li className="shrink-0 h-full w-[272px] select-none border">
+            <div className="w-full rounded-md bg-[#f1f2f4] shadow-md pb-2">
+                <ListHeader data={data}/>
+            </div>
+        </li>
+    );
+};
+export default ListItem;

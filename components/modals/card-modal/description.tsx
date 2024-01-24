b5d2838765e0ab7eeb10908f7b5d2779a5205d68
waleery <walenda1997@gmail.com>
2024-01-24 16:27:29 +0100

    feat(card): add (modal) Description component

---
+++ components/modals/card-modal/description.tsx
@@ -0,0 +1,43 @@
+"use client";
+
+import { Skeleton } from "@/components/ui/skeleton";
+import { CardWithList } from "@/types";
+import { AlignLeft } from "lucide-react";
+
+interface DescriptionProps {
+    data: CardWithList;
+}
+
+const Description = ({ data }: DescriptionProps) => {
+    return (
+        <div className="flex items-start gap-x-3 w-full">
+            <AlignLeft className="h-5 w-5 mt-0.5 text-neutral-700" />
+            <div className="w-full">
+                <p className="font-semibold text-neutral-700 mb-2">
+                    Description
+                </p>
+                <div
+                    role="button"
+                    className="min-h-[78px] bg-neutral-200 text-sm font-medium py-3 px-3.5 rounded-md"
+                >
+                    {data.description || "Add a more detailed description..."}
+                </div>
+            </div>
+
+            {data.description}
+        </div>
+    );
+};
+export default Description;
+
+Description.Skeleton = function DescriptionSkeleton() {
+    return (
+        <div className="flex items-start gap-x-3 w-full">
+            <Skeleton className="h-6 w-6 bg-neutral-200" />
+            <div className="w-full">
+                <Skeleton className="w-24 h-6 mb-2 bg-neutral-200" />
+                <Skeleton className="w-full h-[78px] mb-2 bg-neutral-200" />
+            </div>
+        </div>
+    );
+};
--- components/modals/card-modal/index.tsx
+++ components/modals/card-modal/index.tsx
@@ -8,6 +8,7 @@ import { useQuery } from "@tanstack/react-query";
 
 import { fetcher } from "@/lib/fetcher";
 import { Header } from "./header";
+import Description from "./description";
 
 const CardModal = () => {
     const { id, isOpen, onClose } = useCardModal((state) => state);
@@ -20,6 +21,17 @@ const CardModal = () => {
         <Dialog open={isOpen} onOpenChange={onClose}>
             <DialogContent>
                 {!cardData ? <Header.Skeletorn /> : <Header data={cardData} />}
+                <div className="grid grid-cols-1 md:grid-cols-4 md:gap-4">
+                    <div className="col-span-3">
+                        <div className="w-full space-y-6">
+                            {!cardData ? (
+                                <Description.Skeleton />
+                            ) : (
+                                <Description data={cardData} />
+                            )}
+                        </div>
+                    </div>
+                </div >
             </DialogContent>
         </Dialog>
     );

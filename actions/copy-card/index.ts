0681714157d9d03e46b4bb98d086d39af07c0515
waleery <walenda1997@gmail.com>
2024-01-24 18:43:10 +0100

    feat(serverActions): add copy-card action and schema

---
+++ actions/copy-card/index.ts
@@ -0,0 +1,74 @@
+"use server";
+
+import { auth } from "@clerk/nextjs";
+import { revalidatePath } from "next/cache";
+
+import { db } from "@/lib/db";
+
+import { InputType, OutputType } from "./types";
+import { createSafeAction } from "@/lib/createSafeAction";
+import { CopyCard } from "./schema";
+
+const handler = async (data: InputType): Promise<OutputType> => {
+    const { userId, orgId } = auth();
+
+    if (!userId || !orgId) {
+        return {
+            error: "Unauthorized",
+        };
+    }
+
+    const { id, boardId } = data;
+    let card;
+
+    try {
+        const cardToCopy = await db.card.findUnique({
+            where: {
+                id,
+                list: {
+                    board: {
+                        orgId,
+                    },
+                },
+            },
+        });
+
+        if(!cardToCopy) {
+            return {
+                error: "Card not found.",
+            };
+        }
+
+        const lastCard = await db.card.findFirst({
+            where:{
+                listId: cardToCopy.listId,
+            },
+            orderBy: {
+                order: "desc"
+            },
+            select:{
+                order: true
+            }
+        })
+        const newOrder = lastCard ? lastCard.order + 1 : 1
+
+        card = await db.card.create({
+            data:{
+                title: `${cardToCopy.title} - Copy`,
+                order: newOrder,
+                listId: cardToCopy.listId,
+                description: cardToCopy.description,
+            }
+        })
+    } catch (error) {
+        console.log(error);
+        return {
+            error: "Fail to copy.",
+        };
+    }
+
+    revalidatePath(`/organization/${orgId}}`);
+    return { data: card };
+};
+
+export const copyCard = createSafeAction(CopyCard, handler);
---
+++ actions/copy-card/types.ts
@@ -0,0 +1,10 @@
+import { z } from "zod";
+import { Card } from "@prisma/client";
+
+import { ActionState } from "@/lib/createSafeAction";
+
+import { CopyCard } from "./schema";
+
+
+export type InputType = z.infer<typeof CopyCard>
+export type OutputType = ActionState<InputType, Card>

a26d0349f21050d8e673c1433c34d3116bbe9292
waleery <walenda1997@gmail.com>
2024-01-24 18:56:18 +0100

    feat(card): add copy and delete functionality to CardModal component

--- components/modals/card-modal/actions.tsx
+++ components/modals/card-modal/actions.tsx
@@ -1,15 +1,61 @@
 "use client";
 
+import { copyCard } from "@/actions/copy-card";
+import { deleteCard } from "@/actions/delete-card";
 import { Button } from "@/components/ui/button";
 import { Skeleton } from "@/components/ui/skeleton";
+import { useAction } from "@/hooks/useAction";
+import { useCardModal } from "@/hooks/useCardModal";
 import { CardWithList } from "@/types";
 import { Copy, Trash } from "lucide-react";
+import { useParams } from "next/navigation";
+import { toast } from "sonner";
 
 interface ActionsProps {
     data: CardWithList;
 }
 
 const Actions = ({ data }: ActionsProps) => {
+    const params = useParams()
+    const cardModal = useCardModal()
+
+    const { execute: executeCopyCard, isLoading: isLoadingCopy } = useAction(copyCard,{
+        onSuccess:(data) => {
+            toast.success(`Card ${data.title} copied`)
+            cardModal.onClose()
+        },
+        onError: (error) => {
+            toast.error(error)
+        },
+    });
+    const { execute: executeDeleteCard, isLoading: isLoadingDelete } = useAction(deleteCard,{
+        onSuccess:(data) => {
+            toast.success(`Card ${data.title} deleted`)
+            cardModal.onClose()
+        },
+        onError: (error) => {
+            toast.error(error)
+        },
+    });
+
+    const onCopy = () => {
+        const boardId = params.boardId as string
+
+        executeCopyCard({
+            id: data.id,
+            boardId
+        })
+    }
+    const onDelete = () => {
+        const boardId = params.boardId as string
+
+        executeDeleteCard({
+            id: data.id,
+            boardId
+        })
+    }
+    
+
     return (
         <div className="space-y-2 mt-2">
             <p className="text-xs font-semibold">Actions</p>
@@ -18,6 +64,8 @@ const Actions = ({ data }: ActionsProps) => {
                     variant="gray"
                     className="w-full justify-start"
                     size="inline"
+                    onClick={onCopy}
+                    disabled={isLoadingCopy}
                 >
                     <Copy className="h-4 w-4 mr-2" />
                     Copy
@@ -26,6 +74,8 @@ const Actions = ({ data }: ActionsProps) => {
                     variant="gray"
                     className="w-full justify-start mt-3"
                     size="inline"
+                    onClick={onDelete}
+                    disabled={isLoadingDelete}
                 >
                     <Trash className="h-4 w-4 mr-2" />
                     Trash

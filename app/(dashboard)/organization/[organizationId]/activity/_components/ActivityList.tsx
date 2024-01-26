243595ebc78d8bfaca10f8d0206706452d096cc9
waleery <walenda1997@gmail.com>
2024-01-26 21:36:11 +0100

    feat(organization): Add ActivityPage and ActivityList component

---
+++ app/(dashboard)/organization/[organizationId]/activity/_components/ActivityList.tsx
@@ -0,0 +1,33 @@
+//'use client'
+
+import ActivityItem from "@/components/activityItem";
+import { db } from "@/lib/db";
+import { auth } from "@clerk/nextjs";
+import { redirect } from "next/navigation";
+
+const ActivityList = async () => {
+    const { orgId } = auth();
+
+    if (!orgId) {
+        redirect("/select-org");
+    }
+
+    const auditLogs = await db.auditLog.findMany({
+        where: {
+            orgId,
+        },
+    });
+
+    return (
+        <ol className="space-y-4 mt-4">
+            {/* if last element then <p> is shown */}
+            <p className="hidden last:block text-xs text-center text-muted-foreground">
+                No activity found inside this organization
+            </p>
+            {auditLogs.map((auditLog) => (
+                <ActivityItem key={auditLog.id} data={auditLog} />
+            ))}
+        </ol>
+    );
+};
+export default ActivityList;
---
+++ app/(dashboard)/organization/[organizationId]/activity/page.tsx
@@ -0,0 +1,14 @@
+import { Separator } from "@/components/ui/separator";
+import { Info } from "../_components/info";
+import ActivityList from "./_components/ActivityList";
+
+const ActivityPage = () => {
+    return (
+        <div className="w-full">
+            <Info />
+            <Separator className="my-2"/>
+            <ActivityList />
+        </div>
+    );
+};
+export default ActivityPage;

06bf84a22f94424add40bdec76b8e6a7e2efaaf0
waleery <walenda1997@gmail.com>
2024-01-28 13:07:13 +0100

    feat(stripe): add webhook route for handling Stripe events

--- middleware.ts
+++ middleware.ts
@@ -6,7 +6,7 @@ import { NextResponse } from "next/server";
 // Please edit this to allow other routes to be public as needed.
 // See https://clerk.com/docs/references/nextjs/auth-middleware for more information about configuring your Middleware
 export default authMiddleware({
-    publicRoutes: ["/"],
+    publicRoutes: ["/", "/api/webhook"],
     afterAuth(auth, req) {
 
         // Handle users who aren't authenticated

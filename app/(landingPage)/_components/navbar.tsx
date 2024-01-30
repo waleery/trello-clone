8de8f392a299c6843e2ec0d0a317583e35203112
waleery <walenda1997@gmail.com>
2024-01-30 13:00:43 +0100

    fix: Fix typo in navbar button text

--- app/(landingPage)/_components/navbar.tsx
+++ app/(landingPage)/_components/navbar.tsx
@@ -14,7 +14,7 @@ export const Navbar = () => {
                         <Link href="/sign-in">Login</Link>
                     </Button>
                     <Button size="sm" asChild>
-                        <Link href="/sign-up">Gest Achiveo for free</Link>
+                        <Link href="/sign-up">Get Achiveo for free</Link>
                     </Button>
                 </div>
             </div>
